9812b140dc9b678b64d5fddf8cff9838b89aa82c
Aleksey Kholovchuk <vortexxx192@gmail.com>
2015-12-11 02:44:12 +0300

    Setting date and time for 'serve-contest' job accordingly to data received by Codeforces API.

--- periodic-scripts/check-contests.py
+++ periodic-scripts/check-contests.py
@@ -3,28 +3,45 @@
 import requests
 from crontab import CronTab
 import os
-from config import *
-
-
-current_directory = os.getcwd()
-cron = CronTab(user=True)  # get crontab for current user
+import config
+from datetime import datetime
 
 
 def is_needed_contest(contest):
     return contest["phase"] == "BEFORE" and contest["type"] == "CF"
 
 
-response_json = requests.get(ENDPOINT_TO_CHECK_CONTESTS).json()
-if response_json["status"] == "OK":
-    list_of_contests = response_json["result"]
+def no_cron_job_with_such_comment(cron, comment):
+    return not list(cron.find_comment(comment))
 
-    for contest in list_of_contests:
-        contest_id = str(contest["id"])
 
-        if is_needed_contest(contest) and not list(cron.find_comment(contest_id)):
-            path_to_contest_serving_script = os.path.join(current_directory, CONTEST_SERVING_SCRIPT_NAME)
-            command_to_execute = path_to_contest_serving_script + " " + contest_id
-            job = cron.new(command=command_to_execute, comment=contest_id)
-            job.minutes.every(1)
-            cron.write()
+def add_python_crontab_job(scripts_directory, cron, contest):
+    # create a job that executes given command
+    path_to_contest_serving_script = os.path.join(scripts_directory, config.CONTEST_SERVING_SCRIPT_NAME)
+    command_to_execute = path_to_contest_serving_script + " " + str(contest["id"])
+    job = cron.new(command=command_to_execute, comment=contest_id)
+
+    # set job time
+    startTime = datetime.fromtimestamp(contest["startTimeSeconds"])
+    job.month.on(startTime.month)
+    job.day.on(startTime.day)
+    job.hour.on(startTime.hour)
+    job.minute.on(startTime.minute)
+
+
+if __name__ == "__main__":
+    current_directory = os.getcwd()
+    cron = CronTab(user=True)  # get crontab for current user
+
+    response_json = requests.get(config.ENDPOINT_TO_CHECK_CONTESTS).json()
+    if response_json["status"] == "OK":
+        list_of_contests = response_json["result"]
+
+        for contest in list_of_contests:
+            contest_id = str(contest["id"])
+
+            if is_needed_contest(contest) and no_cron_job_with_such_comment(cron, contest_id):
+                add_python_crontab_job(current_directory, cron, contest)
+
+        cron.write()
 
