39e861e00bfdcb5c9cb1998c7ec75a2b49401f91
Aleksey Kholovchuk <vortexxx192@gmail.com>
2015-12-11 04:12:00 +0300

    Connected redis to serve-contest script and made it run once in 3 seconds.

--- periodic-scripts/config.py
+++ periodic-scripts/config.py
@@ -6,3 +6,6 @@ CONTEST_CHECK_SCRIPT_NAME = "check-contests.py"
 ENDPOINT_TO_CHECK_CONTESTS = "http://codeforces.com/api/contest.list?gym=false"
 CONTEST_SERVING_SCRIPT_NAME = "serve-contest.py"
 
+CONTEST_POSSIBLE_SHIFT_MAX_TIME_SEC = 20*60  # 20 min
+SLEEP_TIME_AFTER_CONTEST_UPDATE = 3
+SERVE_CONTEST_LOG_FILE = "/var/log/nbhext"
--- periodic-scripts/serve-contest.py
+++ periodic-scripts/serve-contest.py
@@ -2,12 +2,45 @@
 
 
 import sys
+import redis
+from time import time, sleep
+import config
+import datetime
 
 
-if len(sys.argv) < 3:
-    raise Exception("Incorrect number of command line arguments. At least 2 required.")
+def do_contest_update(contest_id, redis_server):
+    print("do_contest_update called with args %s %s" % (repr(contest_id), repr(redis_server)))
 
-contest_num = sys.argv[1]
-contest_duration = int(sys.argv[2])
-print("Working for contest %s for %d seconds" % (contest_num, contest_duration))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        raise Exception("Incorrect number of command line arguments. At least 2 required.")
+
+    contest_id = int(sys.argv[1])
+    contest_duration_sec = int(sys.argv[2])
+
+    redis_server = redis.StrictRedis(host="localhost", port=6379, db=0)
+    log_file = open(config.SERVE_CONTEST_LOG_FILE, "a")
+
+    contest_start_time_sec = int(time())
+    contest_end_time = contest_start_time_sec + \
+                       contest_duration_sec + \
+                       config.CONTEST_POSSIBLE_SHIFT_MAX_TIME_SEC
+
+    current_time = contest_start_time_sec
+
+    # main loop begin
+    while current_time <= contest_end_time:
+        try:
+            do_contest_update(contest_id, redis_server)
+        except Exception as ex:
+            print("Exception occured! Please view log file for details.")
+            log_file.write(datetime.datetime.now().isoformat() + " " + repr(ex) + "\n")
+
+        sleep(config.SLEEP_TIME_AFTER_CONTEST_UPDATE)
+
+        current_time = int(time())
+    # main loop end
+    
+    log_file.close()
 
