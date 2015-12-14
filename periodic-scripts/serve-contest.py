289c918bf402c5a283090cd6ada28cde1de451a8
Aleksey Kholovchuk <vortexxx192@gmail.com>
2015-12-14 17:19:22 +0300

    Running serve-contest script while contest is in CODING phase, not until it times out.

--- periodic-scripts/serve-contest.py
+++ periodic-scripts/serve-contest.py
@@ -23,16 +23,16 @@ def get_users(standings, global_ratings):
     return users
 
 
-def do_contest_update(contest_id, redis_server, global_ratings):
-    for (endpoint, redis_suffix) in [(config.OFFICIAL_STANDINGS_ENDPOINT, "official"), \
-                                     (config.UNOFFICIAL_STANDINGS_ENDPOINT, "unofficial")]:
-        # prepare data
-        standings = requests.get(endpoint % (contest_id,)).json()
+def do_contest_update(contest_id, contest_data, global_ratings, redis_server):
+    for data_type, standings in contest_data.items():
+        # Prepare data.
         users = get_users(standings, global_ratings)
         deltas = calculate_rating_change(users)
 
-        # store data to redis
-        redis_key = str(contest_id) + "." + redis_suffix
+        # Store data to redis.
+        # For example, rating deltas for official participants of round 228
+        # will be stored with key '228.official'.
+        redis_key = str(contest_id) + "." + data_type
         redis_value = json.dumps(deltas)
         redis_server.set(redis_key, redis_value)
 
@@ -43,34 +43,33 @@ def get_global_ratings(redis_server):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        raise Exception("Incorrect number of command line arguments. At least 2 required.")
+    if len(sys.argv) < 2:
+        raise Exception("Incorrect number of command line arguments. Contest ID required as a command line argument.")
 
     contest_id = int(sys.argv[1])
-    contest_duration_sec = int(sys.argv[2])
 
     redis_server = redis.StrictRedis(host="localhost", port=6379, db=0)
     log_file = open(config.SERVE_CONTEST_LOG_FILE, "a")
     global_ratings = get_global_ratings(redis_server)
 
-    contest_start_time_sec = int(time())
-    contest_end_time = contest_start_time_sec + \
-                       contest_duration_sec + \
-                       config.CONTEST_POSSIBLE_SHIFT_MAX_TIME_SEC
-
-    current_time = contest_start_time_sec
-
     # main loop begin
-    while current_time <= contest_end_time:
+    while True:
+        contest_data = {
+            "official": requests.get(config.OFFICIAL_STANDINGS_ENDPOINT % (contest_id,)).json(),
+            "unofficial": requests.get(config.UNOFFICIAL_STANDINGS_ENDPOINT % (contest_id,)).json()
+        }
+
+        contest_status = contest_data["official"]["result"]["contest"]["phase"]
+        if contest_status != "CODING":
+            break
+
         try:
-            do_contest_update(contest_id, redis_server, global_ratings)
+            do_contest_update(contest_id, contest_data, global_ratings, redis_server)
         except Exception as ex:
             print("Exception occured! Please view log file for details.")
             log_file.write(datetime.datetime.now().isoformat() + " " + repr(ex) + "\n")
 
         sleep(config.SLEEP_TIME_AFTER_CONTEST_UPDATE)
-
-        current_time = int(time())
     # main loop end
     
     log_file.close()
