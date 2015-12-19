2f13b2659c8506cd9c5990bb8b0f2059dd62b8fe
Aleksey Kholovchuk <vortexxx192@gmail.com>
2015-12-19 02:19:45 +0200

    Moved docstring in utils.py to the top of the file.

--- periodic-scripts/utils.py
+++ periodic-scripts/utils.py
@@ -1,8 +1,9 @@
-from requests import get
-
 """
 This file contains several functions that are useful all across the project.
 """
+
+from requests import get
+
 
 def http_get_json(url, timeout):
     try:
